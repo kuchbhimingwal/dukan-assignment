a5d47ddce4593589d7bc4d8654bedf0846e27833
kuchbhimingwal <shubhammingi@gmail.com>
2024-04-02 08:57:36 -0400

    created the basic frame of the card with just shadow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import RevenueCard from "./conmponents/RevenueCard"
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className="grid grid-cols-3">
+    <RevenueCard title={"Amount pendnig"} amount={"92,321,20"} orderCount={13}/>
+    </div>
   )
 }
 
--- src/conmponents/RevenueCard.jsx
+++ src/conmponents/RevenueCard.jsx
@@ -4,12 +4,29 @@ function RevenueCard(
   {
      title,
      orderCount,
-     
+     amount
   }
 ) {
   return (
-    <div className='bg-white rounded shadow-sm'>
-      
+    <div className='bg-white rounded shadow-sm p-4'>
+      <div className='text-grey=700'>
+        {title}
+        ?
+      </div>
+      <div className='flex justify-between'>
+        <div>
+          {amount}
+        </div>
+        {orderCount ? <div className='flex'>
+          <div>
+            {orderCount} orders(s) 
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+          </svg>
+
+        </div> : null}
+      </div>
     </div>
   )
 }
