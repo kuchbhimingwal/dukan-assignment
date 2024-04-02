ef83c6144fcac2f7a8db9c5b6ff0f06d185343ed
kuchbhimingwal <shubhammingi@gmail.com>
2024-04-02 08:43:55 -0400

    initial comment

---
+++ src/conmponents/RevenueCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function RevenueCard(
+  {
+     title,
+     orderCount,
+     
+  }
+) {
+  return (
+    <div className='bg-white rounded shadow-sm'>
+      
+    </div>
+  )
+}
+
+export default RevenueCard
